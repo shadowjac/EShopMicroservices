d3238441efc1620f61a8cee595e47615f05685d6
Andres Casas <txm.shadow@gmail.com>
2025-05-14 22:49:54 -0500

    configuring project, adding POST products endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,32 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+
+internal record CreateProductRequest(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageUrl,
+    decimal Price);
+
+internal record CreateProductResponse(Guid Id);
+
+public class CreateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products",
+                async (CreateProductRequest request,
+                    ISender sender,
+                    CancellationToken ct) =>
+                {
+                    CreateProductCommand command = request.Adapt<CreateProductCommand>();
+                    CreateProductResult result = await sender.Send(command, ct);
+                    CreateProductResponse response = result.Adapt<CreateProductResponse>();
+
+                    return Results.Created($"/products/{result.Id}", response);
+                }).WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create product")
+            .WithDescription("Create product");
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,41 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct;
+
+internal record CreateProductCommand(string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price
+    ) : ICommand<CreateProductResult>;
+
+internal record CreateProductResult(Guid Id);
+
+internal sealed class CreateProductHandler :  ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    private readonly ILogger<CreateProductHandler> _logger;
+
+    public CreateProductHandler(ILogger<CreateProductHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Creating product {Name}", command.Name);
+        Product product = new()
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+        
+        // 2. save to database
+        // 3. return result
+        
+        return Task.FromResult(new CreateProductResult(product.Id));
+    }
+}
