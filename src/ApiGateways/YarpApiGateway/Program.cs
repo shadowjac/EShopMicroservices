9bb363c8dfd5526d82ebf88a0a4950cd4ae43d01
Andres Casas <txm.shadow@gmail.com>
2025-06-06 23:10:13 -0500

    adding yarp project

---
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+
+//------------------
+var app = builder.Build();
+
+app.MapReverseProxy();
+
+await app.RunAsync();
