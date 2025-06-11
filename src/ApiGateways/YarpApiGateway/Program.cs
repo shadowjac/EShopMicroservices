d38c6e0adb7f4a8ef16a2d1187b4f8e80f9b5216
Andres Casas <txm.shadow@gmail.com>
2025-06-11 18:39:30 -0500

    implementing yarp in docker

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,12 +1,23 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
-
+builder.Services.AddRateLimiter(rlo =>
+{
+    rlo.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
 //------------------
 var app = builder.Build();
+
+app.UseRateLimiter();
 
 app.MapReverseProxy();
 
